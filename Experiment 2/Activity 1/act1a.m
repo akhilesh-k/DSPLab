103247d1c1c30afd68eca5a4f6336b27dfb60eae
Akhilesh Kumar <akhilesh_k@outlook.com>
2018-07-27 11:43:00 +0530

    Add files via upload

---
+++ Experiment 2/Activity 1/act1a.m
@@ -0,0 +1,6 @@
+x = 2*impfn(10,-10,10);
+n=-10:10;
+stem(n,x);
+xlabel('sequence');
+ylabel('value');
+title('Activity 1-A')
---
+++ Experiment 2/Activity 1/act1b.m
@@ -0,0 +1,6 @@
+x = 5*impfn(-10,-20,20)+ 2.5*impfn(20,-20,20);
+n=-20:20;
+stem(n,x);
+xlabel('sequence');
+ylabel('value');
+title('Activity 1-B')
