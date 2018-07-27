8def9c7d073ba036ee8c898c49b6a12ad49195f5
Akhilesh Kumar <akhilesh_k@outlook.com>
2018-07-27 11:51:49 +0530

    Add files via upload

---
+++ Experiment 2/Activity 2/act2a.m
@@ -0,0 +1,6 @@
+x=unitfn(0,-10,10)-unitfn(5,-10,10);
+n=-10:10;
+stem(n,x);
+xlabel('sequence');
+ylabel('value');
+title('Activity 2-A')
--- Experiment 2/Activity 2/unitfn.m
+++ Experiment 2/Activity 2/unitfn.m
@@ -1,3 +1,3 @@
 function[x,n]=unitfn(n0,n1,n2);
- n=n1:n2;
- x=(n-n0)>=0;
+n=n1:n2;
+x=(n-n0)>=0;
